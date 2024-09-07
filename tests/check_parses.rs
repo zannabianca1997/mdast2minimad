da24891753199331fb65a4618872e90bd13556a8
zannabianca1997 <zannabianca199712@gmail.com>
2024-09-07 11:23:52 +0200

    Modified the tests to use the new options

--- tests/check_parses.rs
+++ tests/check_parses.rs
@@ -1,12 +1,12 @@
-use markdown::ParseOptions;
+use mdast2minimad::{md_parse_options, to_minimad};
 
 /// Main test implementation, called for every test source in `sources`
 fn test_source(source: &'static str) {
     // parsing the test with markdown
     let ast =
-        markdown::to_mdast(&source, &ParseOptions::gfm()).expect("Markdown has no syntax errors");
+        markdown::to_mdast(&source, &md_parse_options()).expect("Markdown has no syntax errors");
     // convertint it
-    if let Err(error) = mdast2minimad::to_minimad(&ast) {
+    if let Err(error) = to_minimad(&ast) {
         panic!("Cannot convert: {error}")
     }
 }
